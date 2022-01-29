dd007744947523d4e1bf70ab632cd23d34f7ab63
ndy2 <emrdbs12@gmail.com>
2022-01-29 16:06:57 +0900

    initial commit

---
+++ model/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const userSchema = mongoose.Schema({
+
+    name:{
+        type: String,
+        maxLength: 20
+    },
+    email: {
+        type:String,
+        trim: true
+    },
+    lastname:{
+        type:String,
+        maxlength:20
+    },
+    role:{
+        type:Number,
+        default: 0
+    },
+    image:String,
+    token:{
+        type:String
+    },
+    tokenExp:{
+        type: Number
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = {User}
